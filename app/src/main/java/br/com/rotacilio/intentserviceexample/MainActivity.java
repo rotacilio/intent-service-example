91a24d678f2966565fcf5a4d1462acbeb5e0ed76
Rodrigo Silva <roqls23@gmail.com>
2018-03-13 16:05:52 -0400

    removing IntentServices's implementation

--- app/src/main/java/br/com/rotacilio/intentserviceexample/MainActivity.java
+++ app/src/main/java/br/com/rotacilio/intentserviceexample/MainActivity.java
@@ -1,9 +1,6 @@
 package br.com.rotacilio.intentserviceexample;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Messenger;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
@@ -13,24 +10,18 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import br.com.rotacilio.intentserviceexample.adapters.OrdersAdapter;
-import br.com.rotacilio.intentserviceexample.handlers.HandleOpenedOrders;
 import br.com.rotacilio.intentserviceexample.retrofit.RetrofitConfig;
 import br.com.rotacilio.intentserviceexample.retrofit.models.Order;
 import br.com.rotacilio.intentserviceexample.retrofit.models.OrderItem;
-import br.com.rotacilio.intentserviceexample.services.OpenedOrdersService;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements Callback<Order> {
-
-    public static final String SERVICE_MESSAGE = "br.com.rotacilio.intentserviceexample.SERVICE_MESSAGE";
 
     private List<OrderItem> openedOrders;
     private RecyclerView ordersList;
@@ -45,7 +36,6 @@ public class MainActivity extends AppCompatActivity implements Callback<Order> {
 
         this.initDialog();
         this.fetchOrders();
-//        this.updateOrders();
     }
 
     private void configureRecyclerView() {
@@ -78,18 +68,6 @@ public class MainActivity extends AppCompatActivity implements Callback<Order> {
         return super.onOptionsItemSelected(item);
     }
 
-    public void showResult(List<Order> obj) {
-//        this.openedOrders = obj;
-//        OrdersAdapter adapter = new OrdersAdapter(this.openedOrders);
-//        this.ordersList.setAdapter(adapter);
-//        this.dismissDialog();
-    }
-
-    public void showError(Exception obj) {
-//        Toast.makeText(this, obj.getMessage(), Toast.LENGTH_SHORT).show();
-//        this.dismissDialog();
-    }
-
     private void initDialog() {
         this.dialog = new ProgressDialog(this);
         this.dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
@@ -102,14 +80,6 @@ public class MainActivity extends AppCompatActivity implements Callback<Order> {
     private void dismissDialog() {
         this.dialog.dismiss();
     }
-
-//    private void updateOrders() {
-//        Intent intent = new Intent(this, OpenedOrdersService.class);
-//        intent.putExtra(SERVICE_MESSAGE, "Runed from Intent Service");
-//        intent.putExtra(OpenedOrdersService.MESSENGER_EXTRA,
-//                new Messenger(new HandleOpenedOrders(this)));
-//        startService(intent);
-//    }
 
     private void fetchOrders() {
         Call<Order> call = new RetrofitConfig().getOrdersService().getOpenedOrders();
