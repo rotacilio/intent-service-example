f225bb9fe766ab117e66b6e5d490f863afe0f927
Rodrigo Silva <roqls23@gmail.com>
2018-03-13 11:45:50 -0400

    Implementation of intent service for to consume orders API

--- app/src/main/java/br/com/rotacilio/intentserviceexample/MainActivity.java
+++ app/src/main/java/br/com/rotacilio/intentserviceexample/MainActivity.java
@@ -1,5 +1,8 @@
 package br.com.rotacilio.intentserviceexample;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Messenger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
@@ -14,11 +17,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.rotacilio.intentserviceexample.adapters.OrdersAdapter;
+import br.com.rotacilio.intentserviceexample.handlers.HandleOpenedOrders;
 import br.com.rotacilio.intentserviceexample.models.Order;
+import br.com.rotacilio.intentserviceexample.services.OpenedOrdersService;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String SERVICE_MESSAGE = "br.com.rotacilio.intentserviceexample.SERVICE_MESSAGE";
+
+    private List<Order> openedOrders;
     private RecyclerView ordersList;
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,44 +35,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.ordersList = (RecyclerView) findViewById(R.id.ordersList);
-
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         this.ordersList.setLayoutManager(layoutManager);
-
-        OrdersAdapter adapter = new OrdersAdapter(this.getOrders());
-        this.ordersList.setAdapter(adapter);
 
         DividerItemDecoration dividerItemDecoration =
                 new DividerItemDecoration(this.ordersList.getContext(), layoutManager.getOrientation());
         this.ordersList.addItemDecoration(dividerItemDecoration);
-    }
 
-    private List<Order> getOrders() {
-        List<Order> orders = new ArrayList<>();
-
-        Order order1 = new Order("Pedido 1", "Descrição do pedido 1", "52,99");
-        Order order2 = new Order("Pedido 2", "Descrição do pedido 2", "50,99");
-        Order order3 = new Order("Pedido 3", "Descrição do pedido 3", "45,99");
-        Order order4 = new Order("Pedido 4", "Descrição do pedido 4", "32,99");
-        Order order5 = new Order("Pedido 5", "Descrição do pedido 5", "21,54");
-        Order order6 = new Order("Pedido 6", "Descrição do pedido 6", "15,99");
-        Order order7 = new Order("Pedido 7", "Descrição do pedido 7", "88,99");
-        Order order8 = new Order("Pedido 8", "Descrição do pedido 8", "39,99");
-        Order order9 = new Order("Pedido 9", "Descrição do pedido 9", "46,99");
-        Order order10 = new Order("Pedido 10", "Descrição do pedido 10", "8,99");
-
-        orders.add(order1);
-        orders.add(order2);
-        orders.add(order3);
-        orders.add(order4);
-        orders.add(order5);
-        orders.add(order6);
-        orders.add(order7);
-        orders.add(order8);
-        orders.add(order9);
-        orders.add(order10);
-
-        return orders;
+        this.initDialog();
+        this.updateOrders();
     }
 
     @Override
@@ -77,9 +57,42 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.actionSyncList:
-                Toast.makeText(this, "Atualizar Lista", Toast.LENGTH_SHORT).show();
+                this.initDialog();
+                this.updateOrders();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showResult(List<Order> obj) {
+        this.openedOrders = obj;
+        OrdersAdapter adapter = new OrdersAdapter(this.openedOrders);
+        this.ordersList.setAdapter(adapter);
+        this.dismissDialog();
+    }
+
+    public void showError(Exception obj) {
+        Toast.makeText(this, obj.getMessage(), Toast.LENGTH_SHORT).show();
+        this.dismissDialog();
+    }
+
+    private void initDialog() {
+        this.dialog = new ProgressDialog(this, ProgressDialog.STYLE_SPINNER);
+        this.dialog.setTitle(getString(R.string.title_progress_dialog_orders));
+        this.dialog.setCancelable(false);
+        this.dialog.setIndeterminate(true);
+        this.dialog.show();
+    }
+
+    private void dismissDialog() {
+        this.dialog.dismiss();
+    }
+
+    private void updateOrders() {
+        Intent intent = new Intent(this, OpenedOrdersService.class);
+        intent.putExtra(SERVICE_MESSAGE, "Runed from Intent Service");
+        intent.putExtra(OpenedOrdersService.MESSENGER_EXTRA,
+                new Messenger(new HandleOpenedOrders(this)));
+        startService(intent);
     }
 }
--- app/src/main/java/br/com/rotacilio/intentserviceexample/adapters/holders/OrderViewHolder.java
+++ app/src/main/java/br/com/rotacilio/intentserviceexample/adapters/holders/OrderViewHolder.java
@@ -3,6 +3,7 @@ package br.com.rotacilio.intentserviceexample.adapters.holders;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import br.com.rotacilio.intentserviceexample.R;
 import br.com.rotacilio.intentserviceexample.models.Order;
@@ -11,23 +12,33 @@ import br.com.rotacilio.intentserviceexample.models.Order;
  * Created by roqls on 12/03/2018.
  */
 
-public class OrderViewHolder extends RecyclerView.ViewHolder {
+public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     private TextView title;
     private TextView description;
     private TextView value;
+    private View itemView;
 
     public OrderViewHolder(View itemView) {
         super(itemView);
 
+        this.itemView = itemView;
         this.title = (TextView) itemView.findViewById(R.id.orderTitle);
         this.description = (TextView) itemView.findViewById(R.id.orderDescription);
         this.value = (TextView) itemView.findViewById(R.id.orderValue);
+        this.itemView.setOnClickListener(this);
     }
 
     public void bindView(Order order) {
         this.title.setText(order.getTitle());
         this.description.setText(order.getDescription());
         this.value.setText(order.getValue());
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(view.getContext(),
+                "Nome: " + this.title.getText(),
+                Toast.LENGTH_SHORT).show();
     }
 }
